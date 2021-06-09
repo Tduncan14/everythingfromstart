3dee8cfa822e46937bd0f5df413457199e1e3395
tduncan14 <nbatre1@yahoo.com>
2021-06-08 19:51:12 -0700

    keep moving forward

--- Mastering-Typescript/ch01/hello_typescript.js
+++ Mastering-Typescript/ch01/hello_typescript.js
@@ -2,3 +2,17 @@
 console.log(`hello Typescript`);
 var version = `e6`;
 console.log(`hello ${version} Typescript`);
+var myString = 'this is a string';
+var myBoolean = true;
+var myNumber = 1234;
+var myStringArray = ['first', `second`, `third`];
+var inferredString = "this is a string";
+var inferredNumber = 5;
+// inferredNumber = inferredString;
+function calculate(a, b, c) {
+    return (a * b) + c;
+}
+function printString(a) {
+    console.log(a);
+}
+var returnedValue = printString("this is a string");
--- Mastering-Typescript/ch01/hello_typescript.ts
+++ Mastering-Typescript/ch01/hello_typescript.ts
@@ -4,3 +4,40 @@ var version = `e6`;
 
 
 console.log(`hello ${version} Typescript`)
+
+
+var myString : string = 'this is a string';
+
+
+
+var myBoolean : boolean = true;
+var myNumber : number = 1234;
+var myStringArray: string[] = ['first',`second`, `third`]
+
+
+
+var inferredString = "this is a string";
+var inferredNumber = 5;
+
+
+// inferredNumber = inferredString;
+
+
+function calculate(a:number,b:number,c:number) :number{
+
+     return(a*b) + c
+}
+
+
+
+function printString(a:string) :void {
+
+    console.log(a);
+
+
+
+}
+
+
+var returnedValue : string = printString("this is a string")
+
