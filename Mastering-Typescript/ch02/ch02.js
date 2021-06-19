1ea0f3630148b4c7ece27fdc0c9710b036fc5cfc
tduncan14 <nbatre1@yahoo.com>
2021-06-19 09:22:58 -0700

    keep moving forward

---
+++ Mastering-Typescript/ch02/ch02.js
@@ -0,0 +1,7 @@
+var optionalId = {
+    id: 1
+};
+var optionalIdName = {
+    id: 2,
+    name: "optional name"
+};
--- Mastering-Typescript/ch02/ch02.ts
+++ Mastering-Typescript/ch02/ch02.ts
@@ -23,17 +23,90 @@ interface IOptional {
 
 
 
-let optionalId: IOptional = {
+let optionald: IOptional = {
 
      id:1
 }
 
 
-let optionalIdName: IOptional = {
+let optionaldName: IOptional = {
 
     id: 2,
     name:"optional name"
 
 }
+
+
+interface IWeakType {
+    id?:number,
+    name?:string
+}
+
+
+// let weakTypeNoOverlap: IWeakType = {
+
+//   description:"a descritpion"
+
+
+
+// }
+
+interface  IIdname{
+    id:number,
+    name:string
+}
+
+
+interface IDescValue {
+
+    descr:string,
+    value:number
+}
+
+
+function printNameOrValue(
+    obj: IIdName | IDescValue
+):void{
+
+    if('id'in obj){
+        console.log(`obj.name : ${obj.name}`)
+    }
+
+
+    if('descr' in obj){
+
+        console.log(`obj.value: ${obj.value}`)
+    }
+
+
+}
+
+
+
+interface Iperson {
+    id:number,
+    name:string
+}
+
+
+type PersonPropertyName = keyof Iperson
+
+
+
+function getProperty(key: PersonPropertyName, value: Iperson){
+
+
+    console.log(`${key} = ${value[key]}`)
+}
+
+// getProperty("id",
+//     { id: 1, name: "firstName" }
+// );
+// getProperty("name",
+//     { id: 2, name: "secondName" }
+// );
+// getProperty("telephone",
+//     { id: 3, name: "thirdName" }
+// );
 
 
