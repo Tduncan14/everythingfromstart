daabbe466cab13729572d91725b348c5ea26e4e5
tduncan14 <nbatre1@yahoo.com>
2021-06-22 23:20:16 -0700

    keep moving forward:

--- react-node-typescript/HandsOnTypescript/youwin/src/App.tsx
+++ react-node-typescript/HandsOnTypescript/youwin/src/App.tsx
@@ -1,38 +1,49 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+
+
+
+const reducer = ( state: any, action:any) =>{
+
+  console.log("enteredNameReducer");
+
+  switch(action.type){
+     case "enteredName":
+        if(state.enteredName === action.payload){
+             return state
+        }
+
+       return {...state, enteredName: action.payload}
+
+     case "message":
+        return {...state, message:`Hello, ${action.payload}`}
+
+       default:
+          throw new Error(`Invalid action type ${action.type}`)
+
+       
+       
+  }
+}
+
+
+const initialState = {
+enteredName:"",
+message:""
+}
+
 
 function App() {
 
 
-   const reducer = ( state: any, action:any) =>{
-
-       console.log("enteredNameReducer");
-
-       switch(action.type){
-          case "enteredName":
-             if(state.enteredName === action.payload){
-                  return state
-             }
-
-            return {...state, enteredName: action.payload}
-
-          case "message":
-             return {...state, message:`Hello, ${action.payload}`}
-
-            default:
-               throw new Error(`Invalid action type ${action.type}`)
-
-            
-            
-       }
-   }
+   const [{message, enteredName}, dispatch ] =
+    useReducer(reducer,initialState);
 
 
-   const initialState = {
-     enteredName:"",
-     message:""
-   }
+
+
 
 
   return (
