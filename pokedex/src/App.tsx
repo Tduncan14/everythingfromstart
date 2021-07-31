e0afd992500af14551351823add1072504bb15b8
tduncan14 <nbatre1@yahoo.com>
2021-07-30 21:08:32 -0700

    keep moving forward

--- pokedex/src/App.tsx
+++ pokedex/src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {SWRConfig} from 'swr'
+import PokeContainer from './Pokemon/PokeContainer';
+import fetcher from './Pokemon/fetcher';
+
+
+// styles
+
+import {StyledPokedex,StyledTitle} from './Pokemon/Pokemon.Styled';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <StyledTitle>Pokedex</StyledTitle>
+    
+    <SWRConfig
+    value={{
+    fetcher,
+    suspense: true,
+    }}
+    >
+    <StyledPokedex>
+    <PokeContainer />
+    </StyledPokedex>
+    </SWRConfig>
+    </>
+  )
 }
 
 export default App;
